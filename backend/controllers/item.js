ac6f0120642163704576886d918e3615aca4786f
Malekqandeel <malikqan@gmail.com>
2024-01-05 22:26:00 +0300

    Functions for controllers and routes is under creating, backend

--- backend/controllers/item.js
+++ backend/controllers/item.js
@@ -2,7 +2,9 @@ const ItemModel = require("../models/itemsSchema");
 
 const createItemByUser = (req,res)=>{
     const userId = req.token.userId;
-    const {name,image,description,price,category,user} = req.body;
+    const user = req.params.name
+
+    const {name,image,description,price,category} = req.body;
 
     const newItem = new ItemModel({
         name,image,description,price,category,user
@@ -24,11 +26,61 @@ const createItemByUser = (req,res)=>{
     })
 }
 
-const 
 const updateItemByUser = (req,res)=>{
+    const idItem = req.params.id;
+    const userId = req.token.userId;
+// we don't provide user in Model to get it from token
 
+    const {name,image,description,price,category} = req.body;
+
+    ItemModel.findByIdAndUpdate({_id:idItem},{name,image,description,price,category},{new:true}).then((result)=>{
+        if(!result){
+            return res.status(404).json({
+                success:false,
+                message:'Item is not found'
+            })
+        }
+        res.status(200).json({
+            success:true,
+            message:"Item was Updated",
+            item:result,
+            user:userId
+        })
+    }).catch((err)=>{
+        res.status(500).json({
+            success:false,
+            message:"Server Error",
+            err:err.message
+        })
+    })
 }
 
+const deleteItemByUser = (req,res)=>{
+    const idItem = req.params.id;
+    const userId = req.token.userId;
+
+    ItemModel.findOneAndDelete({_id:idItem}).then((result)=>{
+        if(!result){
+            return res.status(404).json({
+                success:false,
+                message:'Item is not found',
+                user:userId
+            })
+        }
+        res.status(200).json({
+            success:true,
+            message:'Item was deleted'
+        })
+    }).catch((err)=>{
+        res.status(500).json({
+        success:false,
+        message:'Server Error',
+        err:err.message
+        })
+    })
+}
 module.exports={
-    createItemByUser
+    createItemByUser,
+    updateItemByUser,
+    deleteItemByUser
 }
--- backend/routes/item.js
+++ backend/routes/item.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const {createItemByUser} = require("../controllers/item");
+const {createItemByUser,updateItemByUser, deleteItemByUser} = require("../controllers/item");
 
 const authentication = require('../middleware/authentication')
 const authorization = require('../middleware/authorization')
 
 const itemRouter = express.Router();
 
-itemRouter.post("/create",authentication,authorization("ADD"),createItemByUser);
+itemRouter.post("/create/:name",authentication,authorization("ADD"),createItemByUser);
+
+itemRouter.put('/update/:id',authentication,authorization("UPDATE"),updateItemByUser);
+
+itemRouter.delete('/delete/:id',authentication,authorization("DELETE"),deleteItemByUser)
 
 
 module.exports=itemRouter;
