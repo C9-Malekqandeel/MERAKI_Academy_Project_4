bef6be54b037e8e43b175f4d754fe3a9543865be
Malekqandeel <malikqan@gmail.com>
2024-01-10 22:03:19 +0300

    Some Updates on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
+import { Register } from "./components/Register";
 
 function App() {
   return (
     <Routes>
-      
-      <Route path="/Home" element={<Home/>}/>
+
+      {/* <Route path="/Home" element={<Home/>}/> */}
       <Route path="/users/register" element={<Register />} />
-      <Route path="/users/login" element={<Login/>} />
-      <Route path="/About" element={<about/>}/>
+      {/* <Route path="/users/login" element={<Login/>} />
+      <Route path="/About" element={<about/>}/> */}
 
     </Routes>
   );
--- frontend/src/components/Register/index.js
+++ frontend/src/components/Register/index.js
@@ -1,10 +1,112 @@
-import React,{useState} from 'react'
+import React,{useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+
 
 const Register = () => {
-    useS
+    const [userName, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [location, setLocation] = useState("");
+    const [contact, setContact] = useState();
+
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   return (
-    <div>Register</div>
-  )
+    <Form>
+      <Form.Group className="mb-3" controlId="formGroupEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" onchange={(e)=>{
+                setEmail(e.target.value)
+              }}/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" onchange={(e)=>{
+                setPassword(e.target.value)
+              }} />
+      </Form.Group>
+
+
+      <Row className="mb-3">
+        <Form.Group as={Col} md="6" controlId="validationCustomUsername">
+          <Form.Label>Username</Form.Label>
+          <InputGroup hasValidation>
+            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+            <Form.Control
+              type="text"
+              placeholder="Username"
+              aria-describedby="inputGroupPrepend"
+              required
+              //! New Adding
+              onchange={(e)=>{
+                setUserName(e.target.value)
+              }}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please choose a username.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+      </Row>
+
+
+
+      <Row className="mb-3">
+        <Form.Group as={Col} md="6" controlId="validationCustom03">
+          <Form.Label>City</Form.Label>
+          <Form.Control type="text" placeholder="City" required onchange={(e)=>{
+                setLocation(e.target.value)
+              }} />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid city.
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group as={Col} md="6" controlId="validationCustom04">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control type="number" placeholder="State" required onchange={(e)=>{
+                setContact(e.target.value)
+              }}/>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid state.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="3" controlId="validationCustom05">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control type="text" placeholder="Zip" required />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid zip.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      <Form.Group className="mb-3">
+        <Form.Check
+          required
+          label="Agree to terms and conditions"
+          feedback="You must agree before submitting."
+          feedbackType="invalid"
+        />
+      </Form.Group>
+      <Button type="submit">Submit form</Button>
+
+
+    </Form>
+
+    )
 }
 
 export default Register
