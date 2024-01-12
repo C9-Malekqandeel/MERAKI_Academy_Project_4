36f10c0615be7f3537aa4dbb005eb48626a23e93
Malekqandeel <malikqan@gmail.com>
2024-01-12 21:56:02 +0300

    some updates on frontend

--- frontend/src/components/Slides/index.js
+++ frontend/src/components/Slides/index.js
@@ -1,29 +1,38 @@
 import Carousel from 'react-bootstrap/Carousel';
 
-//! Images will be added //carousel
 function Slides() {
   return (
-    <Carousel>
-      <Carousel.Item interval={1000}>
-        
+    <Carousel data-bs-theme="dark">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://media.istockphoto.com/id/1397371374/photo/young-woman-doing-an-online-shopping-for-new-home.jpg?s=612x612&w=0&k=20&c=lF8_a6jVn3eEy6SJ-I4Pfg9AxoHVl-4I0HDyGrfarKE="
+          alt="First slide"
+        />
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <h5>First slide label</h5>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item interval={500}>
-        
-        
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1580828343064-fde4fc206bc6?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Second slide"
+        />
         <Carousel.Caption>
-          <h3>Second slide label</h3>
+          <h5>Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        
-        
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1516321497487-e288fb19713f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fG9ubGluZSUyMHNob3BwaW5nfGVufDB8fDB8fHww"
+          alt="Third slide"
+        />
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h5>Third slide label</h5>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
