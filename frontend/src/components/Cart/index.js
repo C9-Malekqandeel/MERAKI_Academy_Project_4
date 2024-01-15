94bd47f34938d83764e99cb25183ea2806ca2d9a
Malekqandeel <malikqan@gmail.com>
2024-01-15 23:00:49 +0300

    Some updates

--- frontend/src/components/Cart/index.js
+++ frontend/src/components/Cart/index.js
@@ -1,21 +1,57 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import NavBarSignIn from '../NavBarSignIn';
+import Alert from 'react-bootstrap/Alert';
+import axios from "axios"
 
 
 const Cart = () => {
+  const userId = localStorage.getItem("userId");
+  const [order, setOrder] = useState()
+  const token = localStorage.getItem("token");
+
+  useEffect(()=>{
+    axios.get(`http://localhost:5000/orders/user/${userId}`,{
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    }).then((res)=>{ 
+      console.log(res.data.order);
+      setOrder(res.data.order[0])
+  }).catch((err)=>{
+      console.log(err);
+  })
+  },[]);
+
+  console.log(order);
+
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+    <>
+    <NavBarSignIn/>
+    <Alert variant='success'>
+          Are you contact the seller to have these items Or yet!
+        </Alert>
+    <>
+
+    {}
+
+      <Card className="text-center">
+      <Card.Header>Order with </Card.Header>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>elem</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          With supporting text below as a natural lead-in to additional content.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
+      <Card.Footer className="text-muted">2 days ago</Card.Footer>
     </Card>
+    </>
+
+
+
+    </>
 
   )
 }
